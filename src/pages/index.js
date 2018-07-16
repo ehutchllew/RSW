684880cc6441f91f5a962396240bfdac8ae29389
Evan <ehutch7@gmail.com>
2018-07-15 22:32:52 -0400

    added projects and contact pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Landing from './landing'
 import About from './about'
+import Projects from './projects';
 
 const IndexPage = () => (
     <div>
       <Landing />
       <About />
-      <div>
-        <h1 style={{backgroundColor: '#9d979b'}}>HI</h1>
-      </div>
+      <Projects />
     </div>
 )
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './css/projects.css';
 
 export default class Projects extends Component {
     render(){
         return (
-            <div>
+            <div className='projectsContainer'>
                 <h1>Projects</h1>
                 <p>
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
