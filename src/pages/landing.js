5544d89525f3afd31d360209310a04ac896f4a33
Evan <ehutch7@gmail.com>
2018-08-06 23:42:02 -0400

    test

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,16 +2,15 @@ import React from 'react'
 import './css/landing.css'
 
 const Landing = () => {
-    return(
-        <section className="hero">
-            <div className="background-image" />
-            <div className="hero-content-area">
-                <h1>Raysa Suarez Williams</h1>
-                <h3>Image Consulting</h3>
-            </div>
-      </section>
-    )
+  return (
+    <section className="hero">
+      <div className="background-image" />
+      <div className="hero-content-area">
+        <h1>Raysa Suarez Williams</h1>
+        <h3>Image Consulting</h3>
+      </div>
+    </section>
+  )
 }
 
 export default Landing
-  
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './css/projects.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './css/projects.css'
 
 export default class Projects extends Component {
-    constructor(){
-        super();
-    }
-    renderProjects = () => {
-        const { proyectos } = this.props;
-        let classCounter = 1;
-        let classSize = 'small';
-        const allProjects = [1, 2, 3, 4].map((item, index) => {
-            if(index !==0 && classCounter == 2){
-                classSize = classSize == 'small' ? 'large' : 'small';
-                classCounter = 1;
-            } else{
-                classCounter++;
-            }
+  constructor() {
+    super()
+  }
+  renderProjects = () => {
+    const { proyectos } = this.props
+    let classCounter = 1
+    let classSize = 'small'
+    const allProjects = [1, 2, 3, 4].map((item, index) => {
+      if (index !== 0 && classCounter == 2) {
+        classSize = classSize == 'small' ? 'large' : 'small'
+        classCounter = 1
+      } else {
+        classCounter++
+      }
 
-            return(
-                <li className={`projectTile ${classSize}`} style={{backgroundImage: `url(${require(`../assets/mountain${index+1}.jpg`)})`}}></li>
-            )})
-        return allProjects;
-    }
-    render(){
-        return (
-            <div className='projectsContainer'>
-                <h1 className='projectHeader'>Projects</h1>
-                <span />
-                <ul className='projectGrid'>
-                    {this.renderProjects()}
-                </ul>
-            </div>
-        )
-    }
+      return (
+        <li
+          className={`projectTile ${classSize}`}
+          style={{
+            backgroundImage: `url(${require(`../assets/mountain${index +
+              1}.jpg`)})`,
+          }}
+        />
+      )
+    })
+    return allProjects
+  }
+  render() {
+    return (
+      <div className="projectsContainer">
+        <h1 className="projectHeader">Projects</h1>
+        <span />
+        <ul className="projectGrid">{this.renderProjects()}</ul>
+      </div>
+    )
+  }
 }
 
 Projects.propTypes = {
-    proyectos: PropTypes.array
+  proyectos: PropTypes.array,
 }
 
 Projects.defaultProps = {
-    proyectos: new Array(6)
+  proyectos: new Array(6),
 }
-
